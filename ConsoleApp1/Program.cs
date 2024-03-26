4fb589223e9da107db4a7744b2c233819e7f8a5f
Usuario <Usuario@CPD03-M10>
2024-03-26 10:36:26 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            // de um em um até 100
+            
+            for (int i = 1; i <= 100; i++)
+                {
+                    Console.WriteLine(i);
+                }
+
+            // de dois em dois até 100 (apenas pares)
+
+            for (int i = 2; i <= 100; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            // de dois em dois até 100 (apenas impares)
+
+            for (int i = 1; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            // de um em um até -100
+
+            for (int i = 1; i >= 100; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            // de dois em dois até -100 (apenas pares)
+
+            for (int i = 0; i <= 100; i -= 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            // de dois em dois até 100 (apenas impares)
+
+            for (int i = -1; i <= 100; i -= 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
